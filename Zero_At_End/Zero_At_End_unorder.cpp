88f083284f5ff7d48e27a4b9dd36938e087a3cae
Ranveer Singh <workranveersingh@gmail.com>
2026-08-25 01:54:59 +0530

    Refactor Zero_At_End_unorder.cpp for clarity
    Removed unnecessary whitespace and improved code formatting.

--- Zero_At_End/Zero_At_End_unorder.cpp
+++ Zero_At_End/Zero_At_End_unorder.cpp
@@ -1,18 +1,14 @@
 #include<iostream>
 using namespace std ;
-
 int main(){
     int n ;
     cout << "Enter size of array :" ;
     cin >> n ;
-
     int arr[n] ;
-
     cout<< "Enter the Elements in array :" ;
     for(int i = 0 ; i < n ; i++){
         cin>> arr[i] ;
     }
-
     int l=0 , r = n-1 ;
     while(l<r){
         if(arr[l] == 0 && arr[r] != 0){
@@ -27,8 +23,6 @@ int main(){
             r-- ;
         }
     }
-    
-
     cout << "Array after moving zeros to the end: ";
     for(int i=0 ; i<n ; i++){
         cout << arr[i] << " " ;
