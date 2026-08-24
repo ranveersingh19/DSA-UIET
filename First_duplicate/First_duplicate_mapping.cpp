690f84602e9620d6cd6082e0fc2d8f8228a4c114
Ranveer Singh <workranveersingh@gmail.com>
2026-08-25 01:25:28 +0530

    Update First_duplicate_mapping.cpp

--- First_duplicate/First_duplicate_mapping.cpp
+++ First_duplicate/First_duplicate_mapping.cpp
@@ -1,25 +1,19 @@
 #include<iostream>
 #include<unordered_map>
 using namespace std ;
-
 int main(){
     int n ;
     cout << "Enter size of array :" ;
     cin >> n ;
-
     int arr[n] ;
-
     cout<< "Enter the Elements in array :" ;
     for(int i = 0 ; i < n ; i++){
         cin>> arr[i] ;
     }
-
     unordered_map<int,int> mp ;
-
     for(int i=0 ; i<n ; i++){
         mp[arr[i]] = i ;
     }
-
     int pos ;
     bool found = false;
     for(int i=0 ; i<n ; i++){
@@ -29,12 +23,10 @@ int main(){
             break ;
         }
     }
-
     if(found){
         cout<< pos+1 <<"th position";
     }
     else{
         cout<< -1 ;
     }    
-
 }
