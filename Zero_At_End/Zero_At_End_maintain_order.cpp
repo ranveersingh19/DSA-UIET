2061dd7a0ea28b6535ffc5d0fc4e3d5964ade5fe
Ranveer Singh <workranveersingh@gmail.com>
2026-08-25 01:54:28 +0530

    Refactor zero-moving algorithm in C++
    Fix formatting issues and improve zero-moving logic.

--- Zero_At_End/Zero_At_End_maintain_order.cpp
+++ Zero_At_End/Zero_At_End_maintain_order.cpp
@@ -1,20 +1,15 @@
 #include<iostream>
 using namespace std ;
-
 int main(){
     int n ;
     cout << "Enter size of array :" ;
     cin >> n ;
-
     int arr[n] ;
-
     cout<< "Enter the Elements in array :" ;
     for(int i = 0 ; i < n ; i++){
         cin>> arr[i] ;
     }
-
     for(int i = 0 ; i < n ; i++){
-
         if(arr[i] == 0){
             for(int j= i+1 ; j<n ; j++){
                 if(arr[j] != 0){
@@ -23,7 +18,6 @@ int main(){
                 }
             }
         }
-
         cout << "Array after moving zeros to the end: ";
         for(int i=0 ; i<n ; i++){
             cout << arr[i] << " " ;
